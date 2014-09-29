96a29ed0d5d0f14c9c418f0fd476a5fbb49fd97b
RoySegall <roy.segall@gizra.com>
2014-09-29 13:40:08 +0200

    Creating the content with time and images.

--- includes/handlers/node/Event.php
+++ includes/handlers/node/Event.php
@@ -7,11 +7,12 @@ class Event extends CODMigrate {
   public $csvColumns = array(
     array('id', 'ID'),
     array('title', 'Title'),
-    array('field_dates', 'Dates'),
+    array('body', 'Body'),
+    array('start_date', 'Start date'),
+    array('end_date', 'End date'),
     array('field_event_image', 'Image'),
-    array('field_event_program', 'Program'),
-    array('field_event_tickets', 'Tickets'),
     array('field_event_default_session_view', 'Session view'),
+    array('field_event_tickets', 'Tickets'),
     array('uid', 'User'),
   );
 
@@ -22,7 +23,9 @@ class Event extends CODMigrate {
 
     $this->addFieldMapping('title', 'title');
     $this->addFieldMapping('body', 'body');
-    $this->addFieldMapping('field_event_image', 'logo');
+    $this->addFieldMapping('start_date', 'start_date');
+    $this->addFieldMapping('end_date', 'end_date');
+    $this->addFieldMapping('field_event_image', 'field_event_image');
     $this->addFieldMapping('field_event_image:file_replace')
       ->defaultValue(FILE_EXISTS_REPLACE);
     $this->addFieldMapping('field_event_image:source_dir')
@@ -31,5 +34,21 @@ class Event extends CODMigrate {
 
     $this->addFieldMapping('uid', 'uid')
       ->sourceMigration('User');
+  }
+
+  /**
+   * Prepare. Convert the date into timestamp.
+   */
+  public function prepare($entity, $row) {
+    $timestamp = array(
+      'value' => strtotime($row->start_date),
+      'value2' => strtotime($row->end_date),
+      'timezone' => date_default_timezone(),
+      'timezone_db' => date_default_timezone(),
+      'date_type' => 'datestamp',
+    );
+
+    $wrapper = entity_metadata_wrapper('node', $entity);
+    $wrapper->field_dates->set($timestamp);
   }
 }
